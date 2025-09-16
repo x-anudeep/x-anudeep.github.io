c390caa5069965d77a92fc59c99d422304ceae07
Anudeep Cherukupalli <anudeep@Anudeeps-MacBook-Pro.local>
2025-09-16 01:27:46 -0700

    Remove unnecessary frontend and backend folders

--- studentmanagement(Spring)/studentmanagement/src/main/java/com/example/studentmanagement/repository/AssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.studentmanagement.repository;
-
-import com.example.studentmanagement.model.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-}
